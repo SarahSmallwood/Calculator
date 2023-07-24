c18701940aab0874676379057aa0b5a8eab42ef4
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-07-24 13:21:08 -0400

    calc

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const uploadToScreen = (number) => {
    
 
 // Clears Screen 
-    const CalculateTerms = (x,x,sign) => {
+    const calculateTerms = (x,x,sign) => {
         if(sign === "+"){
             screen.innerHTML = parseInt(x) +  
             parseInt(y);
@@ -27,19 +27,35 @@ const uploadToScreen = (number) => {
         }else if (sign === "%"){
                     screen.innerHTML = parseInt(x) /  
                     parseInt(y);
-        } else if (sign === "CLR"){
-                    screen.innerHTML = clearScreen();
+        }else {
+            alert("error");
         }
 
     };
 
     const splitInput = () => {
-
-
         let nput = screen.textContent;
 
         console.log(nput);
         //splits two container
-        if(nput.includes)
-
-    }
+        if (nput.includes("+")) {
+            let solve = nput.split("+");
+            console.log("input:", solve);
+            calculateTerms(solve[0], solve[1], "+");
+          } else if (nput.includes("-")) {
+            let solve = nput.split("-");
+            console.log("input:", solve);
+            calculateTerms(solve[0], solve[1], "-");
+          } else if (nput.includes("X")) {
+            let solve = nput.split("X");
+            console.log("input:", solve);
+            calculateTerms(solve[0], solve[1], "X");
+          } else if (nput.includes("%")) {
+            let solve = nput.split("%");
+            console.log("input:", solve);
+            calculateTerms(solve[0], solve[1], "%");
+          } else {
+            alert("You need to add a Symbol in order to calculate");
+          }
+          //
+        };
